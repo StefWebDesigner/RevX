ce5dd568ccc67a23ab75362414a49388b6a004df
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-04-08 10:43:31 -0400

    User Report Made

--- projectapp/src/components/admin/admin-groups/UserPanel.js
+++ projectapp/src/components/admin/admin-groups/UserPanel.js
@@ -22,7 +22,7 @@ const UserPanel = () => {
                     </div>
 
                     {/*OVERALL FACT ROW*/}
-                    <div className="row">
+                    <div className="row mb-3">
 
                         {/*FACT 1*/}
                         {/*COL FOR CARD*/}
@@ -101,11 +101,12 @@ const UserPanel = () => {
 
 
                     {/*GET ALL USERS ROW*/}
-                    <div className="row">
+                    <div className="row mb-3">
+
 
                         {/*COL FOR CARD*/}
                         <div className="col-xl-12 col-lg-12 col-md-12">
-                            <div className="card shadow h-100 mt-5">
+                            <div className="card shadow h-100">
                                 <div className="card-body">
                                     {/*CARD ROW*/}
                                     <div className="row no-gutters align-items-center">
@@ -144,9 +145,35 @@ const UserPanel = () => {
                                                         </aside>
                                                     }
 
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/*END CARD ROW*/}
+                                    </div>
+                                </div>
+                            </div>
+                            {/*COL END*/}
+                        </div>
+                        {/*OVERALL FACT ROW*/}
+                        </div>
 
 
+                    <div className="row">
 
+                        {/*GRAPH*/}
+                        {/*COL FOR CARD*/}
+                        <div className="col-xl-6 col-lg-6 col-md-6">
+                            <div className="card shadow h-100">
+                                <div className="card-body ">
+                                    {/*CARD ROW*/}
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="fact-body">
+                                                <div className="text-center text-xs font-weight-bold text-uppercase">
+                                                    User Graph :
+                                                </div>
+                                                <div className=" text-center h5 mb-0 font-weight-bold text-gray-800">
+                                                    (Take from from DS)
                                                 </div>
                                             </div>
                                         </div>
@@ -158,9 +185,48 @@ const UserPanel = () => {
                         </div>
 
 
+                        {/*REPORT USER*/}
+                        {/*COL FOR CARD*/}
+                        <div className="col-xl-6 col-lg-6 col-md-6">
+                            <div className="card shadow h-100">
+                                <div className="card-body">
+                                    {/*CARD ROW*/}
+                                    <div className="row no-gutters align-items-center">
+                                        <div className="col mr-2">
+                                            <div className="fact-body">
+                                                <div className="text-center text-xs font-weight-bold text-uppercase">
+                                                    Report Users :
+                                                </div>
+                                                <div className=" text-center h5 mb-0 font-weight-bold text-gray-800 mb-3">
+                                                    Table has the following pending reported Users
+                                                </div>
+                                                <div className=" text-center h5 mb-0 font-weight-bold text-gray-800">
+
+                                                    {/*A PLACE HOLDER FOR THE REAL CONTENT*/}
+                                                    <div className="row">
+                                                        <table>
+                                                            <thead>
+                                                                <th className="col-2">Id</th>
+                                                                <th className="col-2">User</th>
+                                                                <th className="col-2">Complaint</th>
+                                                                <th className="col-2">Ignore</th>
+                                                                <th className="col-2">Ban</th>
+                                                            </thead>
+                                                        </table>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/*END CARD ROW*/}
+                                    </div>
+                                </div>
+                            </div>
+                            {/*COL END*/}
+                        </div>
+
                         {/*OVERALL FACT ROW*/}
                     </div>
-
 
 
 
