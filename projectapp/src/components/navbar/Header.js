493e7d30274c82ebabc6b4961a4e81eabf6903f9
samuelsholib <samuelsholib12@gmail.com>
2022-04-16 12:32:24 -0700

    account items

--- projectapp/src/components/navbar/Header.js
+++ projectapp/src/components/navbar/Header.js
@@ -25,54 +25,8 @@ function Header() {
                         <Col></Col>
                         <Col xs={4}>{user ? <h4>Welcome, {user.firstname}</h4> : <h4>Welcome</h4>}</Col>
                     {/*<Following/>*/}
-                        <Col >
-                           <Link to='/searchResults'>Search page</Link>
-//                             <InputGroup>
-//                                 <InputGroup.Text bsPrefix="search-icon">
-//                                     <img src="../../../images/mg-white.svg" className="icon" alt=""/>
-//                                 </InputGroup.Text>
-//                                 <Form.Select id="searchtype" bsPrefix="search" defaultValue={"name"}>
-//                                     <option value="name">Name</option>
-//                                     <option value="location">Location</option>
-//                                 </Form.Select>
-//                                 <FormControl
-//                                     placeholder="Search..."
-//                                      onChange={(e) => searchResults(e.target.value)}
-
-//                                     aria-label="Search"
-//                                     aria-describedby="basic-addon2"
-// //                                 />
-//                             </InputGroup>
-                                <InputGroup.Text ><img src="../../../images/mg-black.svg" className="icon" alt=""/></InputGroup.Text>
-
-                                <FormSelect onChange={(e)=>{
-                                    console.log(e.target.value)
-                                    //BY SELECTED DROP DOWN OPTION, THE VALUE IS SET FOR THE FOLLWING PART
-                                    setSearchCategory(e.target.value);
-                               }}>
-                                    //BUTTON SEARCH OPTIONS
-                                    <option>Username</option>
-                                    <option>Location</option>
-                                </FormSelect>
-                                <FormControl
-                                    placeholder="Search..."
-                                    // LOOK FOR CHANGE AND IT TO SEARCH FOR BUTTON TO ACCESS
-                                     onChange={(e) => {
-
-                                         setSearch(e.target.value)
-                                     }}/>
-                            <button onClick={()=>{
-                                //MATCHING WITH CONDITIONS & APPLY SEARCH STATE TO REULTS
-                                console.log(searchCategory)
-                                if(searchCategory==="Username"){
-                                    searchResultsUsername(search)
-
-                                }
-                                if(searchCategory==="Location"){
-                                searchResultsLocation(search)
-                                }
-                            }}>Search</button>
-                        </Col>
+                        
+                        
                     </Row>
                     <Row>
 
